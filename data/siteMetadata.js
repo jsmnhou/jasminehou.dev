7d50663e68c68b2d71a458c4f8ee4cc4a1ee2753
jsmnhou <jsmnhou@umich.edu>
2023-05-16 21:43:57 -0700

    Initial push.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,10 +2,10 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Jasmine Hou Website',
+  author: 'Jasmine Hou',
+  headerTitle: 'Jasmine Hou',
+  description: 'A personal website created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
