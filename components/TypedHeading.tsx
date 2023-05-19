361e7445b4a3773c380d25579cfbc6e526190221
jsmnhou <jsmnhou@umich.edu>
2023-05-19 15:12:55 -0700

    typing animation

---
+++ components/TypedHeading.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from 'react'
+import Typed from 'typed.js'
+// import { Twemoji } from '../Twemoji'
+
+export function TypedHeading() {
+  const el = useRef(null)
+  // let typed = React.useRef(null)
+
+  useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ["Hi, I'm Jasmine!"],
+      startDelay: 10,
+      typeSpeed: 50,
+      backSpeed: 10,
+      backDelay: 1500,
+      loop: true,
+    })
+    return () => typed.destroy()
+  }, [])
+
+  return (
+    <div className="font-sans text-3xl font-semibold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+      
+      <span ref={el} />
+    </div>
+  )
+}
