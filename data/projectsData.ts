595e39460790231fff75f70f060151f3c78e1f0b
jsmnhou <jsmnhou@umich.edu>
2023-05-20 02:54:49 -0700

    project cover photos

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,23 +2,23 @@ const projectsData = [
   {
     title: 'jasminehou.dev',
     description: `My personal website created with Next.js and Tailwind.css.`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: '/static/images/website-cover.png',
     href: 'https://github.com/jsmnhou/jasmine-website-v2',
-    projectType: 'personal'
+    projectType: 'personal',
   },
   {
     title: 'Mental Health/COVID-19 Analysis',
     description: `Investigating effects of COVID-19 on student mental health with NumPy, Pandas, and more.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/covid-cover.png',
     href: 'https://github.com/jsmnhou/mental-health/blob/main/final_mental_health_during_COVID19.ipynb',
-    projectType: 'personal'
+    projectType: 'personal',
   },
   {
     title: 'youtube-dl',
     description: `Open source contributions to the popular command-line program for downloading videos.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: '/static/images/youtube-dl-cover.jpeg',
     href: 'https://github.com/ytdl-org/youtube-dl',
-    projectType: 'oss'
+    projectType: 'oss',
   },
 ]
 
